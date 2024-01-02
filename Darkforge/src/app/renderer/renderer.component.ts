3e3d3099e125892771e0079d15d13876aec48c88
EmrahVeladzic <148920444+EmrahVeladzic@users.noreply.github.com>
2024-01-02 06:55:13 +0100

    Implementing WebGL

---
+++ Darkforge/src/app/renderer/renderer.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-renderer',
+  standalone: true,
+  imports: [],
+  templateUrl: './renderer.component.html',
+  styleUrl: './renderer.component.css'
+})
+export class RendererComponent {
+
+
+
+
+}
