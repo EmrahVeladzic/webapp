c3cb93ef6f983db592ec4d88f6ece432bf7b6506
EmrahVeladzic <148920444+EmrahVeladzic@users.noreply.github.com>
2024-02-05 19:09:24 +0100

    Added resize callback

--- Darkforge/src/app/renderer/webgl_service/web-gl.service.ts
+++ Darkforge/src/app/renderer/webgl_service/web-gl.service.ts
@@ -1,8 +1,9 @@
 import { HttpRequest } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HostListener, Injectable, numberAttribute } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { glMatrix, mat4 } from 'gl-matrix';
+import { withNoHttpTransferCache } from '@angular/platform-browser';
 
 
 @Injectable({
@@ -14,31 +15,57 @@ export class WebGLService {
   private fragCode! :string;
   private vertexShad :WebGLShader | null = null;
   private fragmentShad :WebGLShader | null = null;
+  private GLProgram : WebGLProgram  | null  = null;
+  private WMatLoc :any;
+  private VMatLoc :any;
+  private PMatLoc :any;
+  private wMat  : any;
+  private vMat  : any;
+  private pMat  : any;
+  private verts : any;
+  private inds  : any;
 
   constructor(private http :HttpClient) { 
 
   }
 
-  render(){
+  @HostListener('window:resize',['$event'])
+  onresize(event:Event):void{
+
+    
 
     if(this.gl){
-    var GLProgram = this.gl.createProgram();
-     
+    this.gl.canvas.width=window.innerWidth;
+    this.gl.canvas.height=window.innerHeight;
 
-    if(GLProgram){
+    this.gl.viewport(0,0,this.gl.canvas.width,this.gl.canvas.height);
+    
+    
+
+    }   
+  }
+
+  setup(){
+
+    if(this.gl){
+    this.GLProgram = this.gl.createProgram();   
+    
+   
+
+    if(this.GLProgram){
 
       if(this.vertexShad){
-        this.gl.attachShader(GLProgram,this.vertexShad);
+        this.gl.attachShader(this.GLProgram,this.vertexShad);
       }
     
       if(this.fragmentShad){
-        this.gl.attachShader(GLProgram,this.fragmentShad);
+        this.gl.attachShader(this.GLProgram,this.fragmentShad);
       }                
 
-      this.gl.linkProgram(GLProgram);
+      this.gl.linkProgram(this.GLProgram);
     }
    
-    var verts = 
+    this.verts = 
 	[ // X, Y, Z           R, G, B
 		// Top
 		-1.0, 1.0, -1.0,   0.5, 0.5, 0.5,
@@ -77,7 +104,7 @@ export class WebGLService {
 		1.0, -1.0, -1.0,    0.5, 0.5, 1.0,
 	];
    
-  var inds =
+  this.inds =
 	[
 		// Top
 		0, 1, 2,
@@ -106,17 +133,17 @@ export class WebGLService {
 
     var vBuffer = this.gl.createBuffer();
     this.gl.bindBuffer(this.gl.ARRAY_BUFFER, vBuffer);
-    this.gl.bufferData(this.gl.ARRAY_BUFFER,new Float32Array(verts),this.gl.STATIC_DRAW);
+    this.gl.bufferData(this.gl.ARRAY_BUFFER,new Float32Array(this.verts),this.gl.STATIC_DRAW);
 
 
     var iBuffer = this.gl.createBuffer();
     this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER,iBuffer);
-    this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER,new Uint16Array(inds),this.gl.STATIC_DRAW);
+    this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER,new Uint16Array(this.inds),this.gl.STATIC_DRAW);
 
       
       
-      var pos = this.gl.getAttribLocation(GLProgram!,'vPosition');
-      var clr = this.gl.getAttribLocation(GLProgram!,'vColor');
+      var pos = this.gl.getAttribLocation(this.GLProgram!,'vPosition');
+      var clr = this.gl.getAttribLocation(this.GLProgram!,'vColor');
 
 
 
@@ -129,48 +156,92 @@ export class WebGLService {
 
       
 
-      this.gl.useProgram(GLProgram);
+      this.gl.useProgram(this.GLProgram);
 
       
 
-      var WMatLoc =this.gl.getUniformLocation(GLProgram!,'worldMat');
-      var VMatLoc =this.gl.getUniformLocation(GLProgram!,'viewMat');
-      var PMatLoc =this.gl.getUniformLocation(GLProgram!,'projMat');
+       this.WMatLoc =this.gl.getUniformLocation(this.GLProgram!,'worldMat');
+       this.VMatLoc =this.gl.getUniformLocation(this.GLProgram!,'viewMat');
+       this.PMatLoc =this.gl.getUniformLocation(this.GLProgram!,'projMat');
 
-      var wMat = new Float32Array(16);
-      var vMat = new Float32Array(16);
-      var pMat = new Float32Array(16);
+       this.wMat = new Float32Array(16);
+       this.vMat = new Float32Array(16);
+       this.pMat = new Float32Array(16);
 
       
     
-      mat4.identity(wMat);
-      mat4.lookAt(vMat,[0.0,0.0,-7.5],[0.0,0.0,0.0],[0.0,1.0,0.0]);
-      mat4.perspective(pMat,glMatrix.toRadian(45),this.gl.canvas.width/this.gl.canvas.height,0.1,1000.0);
+     
 
-      this.gl.uniformMatrix4fv(WMatLoc,false,wMat);
-      this.gl.uniformMatrix4fv(VMatLoc,false,vMat);
-      this.gl.uniformMatrix4fv(PMatLoc,false,pMat);
+      this.render();
+      
+
+    }
+
+
+  }
+
+  render(){
+
+    if(this.gl){    
+    
+
+      var vBuffer = this.gl.createBuffer();
+      this.gl.bindBuffer(this.gl.ARRAY_BUFFER, vBuffer);
+      this.gl.bufferData(this.gl.ARRAY_BUFFER,new Float32Array(this.verts),this.gl.STATIC_DRAW);
+
+
+      var iBuffer = this.gl.createBuffer();
+      this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER,iBuffer);
+      this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER,new Uint16Array(this.inds),this.gl.STATIC_DRAW);
+
         
+        
+      var pos = this.gl.getAttribLocation(this.GLProgram!,'vPosition');
+      var clr = this.gl.getAttribLocation(this.GLProgram!,'vColor');
+
+
+
+      this.gl.vertexAttribPointer(pos,3,this.gl.FLOAT,false,6*Float32Array.BYTES_PER_ELEMENT,0*Float32Array.BYTES_PER_ELEMENT);
+      this.gl.vertexAttribPointer(clr,3,this.gl.FLOAT,false,6*Float32Array.BYTES_PER_ELEMENT,3*Float32Array.BYTES_PER_ELEMENT);
+
+
+      this.gl.enableVertexAttribArray(pos);
+      this.gl.enableVertexAttribArray(clr);
+
+        
+
+      this.gl.useProgram(this.GLProgram);
+
+    
+        
+      
+      mat4.identity(this.wMat);
+      mat4.lookAt(this.vMat,[0.0,0.0,-7.5],[0.0,0.0,0.0],[0.0,1.0,0.0]);
+      mat4.perspective(this.pMat,glMatrix.toRadian(45),this.gl.canvas.width/this.gl.canvas.height,0.1,1000.0);
+
+      this.gl.uniformMatrix4fv(this.WMatLoc,false,this.wMat);
+      this.gl.uniformMatrix4fv(this.VMatLoc,false,this.vMat);
+      this.gl.uniformMatrix4fv(this.PMatLoc,false,this.pMat);
+          
       var ang = 0;
       var idM = new Float32Array(16);
-      mat4.identity(idM);
+      mat4.identity(idM);   
+       
 
-      var self = this;
-      var repeat = function(){
+      ang=performance.now()/1000/6*2*Math.PI;
+      mat4.rotate(this.wMat,idM,ang,[0.7,1.0,0.3]);
+      this.gl!.uniformMatrix4fv(this.WMatLoc,false,this.wMat);
 
-        ang=performance.now()/1000/6*2*Math.PI;
-        mat4.rotate(wMat,idM,ang,[0.7,1.0,0.3]);
-        self.gl!.uniformMatrix4fv(WMatLoc,false,wMat);
+      this.gl!.clearColor(0.175,0.175,0.175,1.0);
+      this.gl!.clear(this.gl!.COLOR_BUFFER_BIT|this.gl!.DEPTH_BUFFER_BIT);
 
-        self.gl!.clearColor(0.175,0.175,0.175,1.0);
-        self.gl!.clear(self.gl!.COLOR_BUFFER_BIT|self.gl!.DEPTH_BUFFER_BIT);
-
-        self.gl!.drawElements(self.gl!.TRIANGLES,inds.length,self.gl!.UNSIGNED_SHORT,0);
+          
+      this.gl!.drawElements(this.gl!.TRIANGLES,this.inds.length,this.gl!.UNSIGNED_SHORT,0);
 
 
-        requestAnimationFrame(repeat);
-      }
-      requestAnimationFrame(repeat);
+        
+        
+      requestAnimationFrame(this.render.bind(this));
 
     }
 
@@ -228,12 +299,19 @@ export class WebGLService {
     }
   }
 
+  
+ 
   initialise(canvas : HTMLCanvasElement){
     this.gl=canvas.getContext("webgl2",{antialias:true});
     if(this.gl){
+      this.gl.canvas.width=window.innerWidth;
+      this.gl.canvas.height=window.innerHeight;
       this.gl.viewport(0,0,this.gl.canvas.width,this.gl.canvas.height);
       
-      this.gl.enable(this.gl.DEPTH_TEST);
+      this.gl.enable(this.gl.DEPTH_TEST);         
+
+      window.addEventListener('resize', (event) => this.onresize(event));
+
       
 
       this.vertexShad = this.gl.createShader(this.gl.VERTEX_SHADER);
@@ -256,7 +334,7 @@ export class WebGLService {
           this.fragCode = content;
           this.compile_fragment();
          
-          this.render();
+          this.setup();
         },
         error: (error) => {
           console.error('Error loading shader:', error);
@@ -267,4 +345,8 @@ export class WebGLService {
     }
     else{}
   }
+
+  
+
 }
+
